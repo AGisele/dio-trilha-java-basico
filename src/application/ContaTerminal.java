6db1544b4d09f86d5e860bf40ba6f33238ef0f00
Gisele Araújo <araujocgisele@gmail.com>
2023-08-18 14:27:41 -0300

    Project created

---
+++ src/application/ContaTerminal.java
@@ -0,0 +1,13 @@
+package application;
+
+public class ContaTerminal {
+
+	public static void main(String[] args) {
+		int numero;
+		String agencia;
+		String nomeCliente;
+		double saldo;
+
+	}
+
+}
